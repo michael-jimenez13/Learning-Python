09e07022877a504c01fa1303616f90f600d30a30
Michael Jimenez <91153157+michael-jimenez13@users.noreply.github.com>
2022-04-25 16:07:30 -0500

    Add files via upload

---
+++ hello1.py
@@ -0,0 +1,11 @@
+class Point:
+    def _init_(self, name);
+        self.name = name
+        
+    def talk(self):
+        print(f"Hi, I am {self.name}")
+        
+               
+John = Point("john smith")
+point.talk()
+ 
